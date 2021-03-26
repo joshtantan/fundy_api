a3f05081b2e55195d0b8a8aac69350b28a5e0630
Joshua Tan <josh.tan.tann@gmail.com>
2021-03-25 21:05:36 -0700

    Add get all users query
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- db/queries.js
+++ db/queries.js
@@ -6,3 +6,12 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
+
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
